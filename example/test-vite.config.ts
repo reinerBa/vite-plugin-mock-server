242c0a072beea183f6c9eb86c8d0c4731d12cebf
reinerBa <4329883+reinerBa@users.noreply.github.com>
2024-01-13 23:37:35 +0100

    use the same test cases in both examples

--- example/test-vite.config.ts
+++ example/test-vite.config.ts
@@ -1,41 +1,37 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, UserConfigExport, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import mockSimple from '../src'
 import fetch from 'node-fetch-commonjs'
+import routes0 from '../example2/mockRoutes'
 import routes1 from './mock/es.mock'
 import routes2 from './mock/cjs.mock'
 import routes3 from './mock/apis/es2.mock'
 import routes4 from './mock/apis/cjs2.mock'
 
 const port = 8000
-export default defineConfig({
+let errors: string[]
+let responseText, responseJson, userId
+export default async ({ command, mode }: ConfigEnv): Promise<UserConfigExport> => {
+  
+  const runTestsI =await import('../example2/testMockRoutes')
+  const runTests = runTestsI.default
+  const stopdevI =await import('vite-plugin-stopdev')
+  const stopdev = stopdevI.default
+  return {
   server: { port },
   plugins: [
     vue(),
-    mockSimple([...routes1, ...routes2, ...routes3(), ...routes4]),
-    {
-      name: `vite-plugin-stop`,
-      apply: 'serve',
-      configureServer(server) {
-        setTimeout(async ()=> {
-          const resp = await fetch(`http://127.0.0.1:${port}/api/test1/1`)
-          const responseText = await resp.text()
-
-          const resp2 = await fetch(`http:/127.0.0.1:${port}/api/test1/users/octoape`)
-          const responseJson: any = await resp2.json()
-
-          const userId = responseJson.params.userId
-          
-          await server.close()
-          if(responseText !== 'Hello world!/api/test1/1' || userId !== "octoape") 
-            throw new Error('wrong mock response')
-          else 
-          console.log('\x1b[32m ✓ correct mock response \x1b[0m');
-        
-          setTimeout(process.exit, 1e3)
-        }
-        , 3000)
-      }
-    }
-  ]
-})
+    mockSimple([...routes0, ...routes1, ...routes2, ...routes3(), ...routes4]),
+    stopdev({
+      afterIdle: 2e3,
+      async beforeStop(){
+        errors = await runTests(`http://127.0.0.1:${port}`, fetch)
+      },
+      async afterStop(){
+        errors.forEach(console.error)
+        if(errors.length)
+          throw new Error('wrong mock response')
+      }         
+    })
+  ]}
+}
